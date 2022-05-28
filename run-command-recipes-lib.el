db306be1eb1ec4359f1f00b35b46b86e1b325511
semenInRussia <hrams205@gmail.com>
2022-05-28 15:35:17 +0300

    Initial commit for standard library of this package

---
+++ run-command-recipes-lib.el
@@ -0,0 +1,62 @@
+;;; run-command-recipes-lib.el --- Standard library for `run-command-recipes' -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 Free Software Foundation, Inc.
+
+;; Author: semenInRussia <hrams205@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Standard library for `run-command-recipes'
+
+;;; Code:
+
+(require 'dash)
+(require 'f)
+(require 's)
+
+(defun run-command-recipes-lib-compose-recipes (&rest recipes)
+  "Return composition of all RECIPES."
+  (->> recipes (-map #'funcall) (apply '-concat)))
+
+(defcustom run-command-recipes-lib-bind-variables
+  '(("file-name" . (buffer-file-name))
+    ("file-name-no-ext" . (--when-let (buffer-file-name) (f-no-ext it))))
+  "Variables' usages for :command-line in recipes of `run-command'.
+Alist in which keys are fragments of strings, values are Lisp sexps which will
+replace source string.  In string at :command-line {file-name} will be replaced
+to value of this alist at key \"file-name\""
+  :type '(alist :key-type string :value-type list))
+
+(defun run-command-recipes-lib-bind-in-recipe (body)
+  "Replace some things in :command-line of evaluated BODY."
+  (->>
+   body
+   (--map
+    (plist-map it
+               :command-line #'run-command-recipes-lib--change-command-line))))
+
+(defun run-command-recipes-lib--change-command-line (command-line)
+  "Replace some fragments of COMMAND-LINE to respective things."
+  (let ((replacements
+         (->>
+          run-command-recipes-lib-bind-variables
+          (--map
+           (cons
+            (s-concat "{" (car it) "}")
+            (eval (cdr it)))))))
+    (s-replace-all replacements command-line)))
+
+(provide 'run-command-recipes-lib)
+;;; run-command-recipes-lib.el ends here
