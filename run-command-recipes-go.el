e50eb4a605b3b296cd2964d39510676b1c10862f
semenInRussia <hrams205@gmail.com>
2024-07-07 21:59:53 +0300

    [Go] try to avoid usage of my lib

--- run-command-recipes-go.el
+++ run-command-recipes-go.el
@@ -30,106 +30,132 @@
 (require 'f)
 (require 'run-command-recipes-lib)
 
-
 (defcustom run-command-recipes-go-major-modes '(go-mode)
   "List of major modes in which should work `run-command' recipe for Golang."
   :type '(repeat symbol)
   :group 'run-command-recipes)
 
-(defun run-command-recipes-go ()
-  "Recipe of `run-command' for go."
-  (run-command-recipes-lib-build
-   (when (run-command-recipes-go-p)
-     (list
-      (list
-       :command-name "go-build-project"
-       :command-line "go build"
-       :display "Go: compile packages and dependencies")
-      (list
-       :command-name "go-build"
-       :display "Go: build file"
-       :command-line "go build {file-name}")
-      (list
-       :command-name "go-run"
-       :display "Go: compile, execute file"
-       :command-line "go run {file-name}")
-      (list
-       :command-name "go-run-project"
-       :command-line "go run"
-       :display "Go: compile, execute program")
-      (list
-       :command-name "go-bug"
-       :command-line "go bug"
-       :display "Go: start a bug report")
-      (list
-       :command-name "go-clean"
-       :command-line "go clean"
-       :display "Go: remove object files and cached files")
-      (list
-       :command-name "go-clean-recursively"
-       :command-line "go clean -r"
-       :display "Go: remove object files and cached files recursively")
-      (list
-       :command-name "go-env"
-       :command-line "go env"
-       :display "Go: print Go environment information")
-      (list
-       :command-name "go-fix"
-       :command-line "go fix"
-       :display "Go: update packages to use new APIs")
-      (list
-       :command-name "go-fmt-project"
-       :command-line "go fmt"
-       :display "Go: gofmt (reformat) package sources")
-      (list
-       :command-name "go-fmt"
-       :command-line "go fmt {filename}"
-       :display "Go: gofmt (reformat) file")
-      (list
-       :command-name "go-generate"
-       :command-line "go generate"
-       :display "Go: generate Go files by processing source")
-      (list
-       :command-name "go-get"
-       :command-line "go get"
-       :display "Go: add dependencies to current module and install them")
-      (list
-       :command-name "go-install"
-       :command-line "go install"
-       :display "Go: compile and install packages and dependencies")
-      (list
-       :command-name "go-list"
-       :command-line "go list"
-       :display "Go: list packages or modules")
-      (list
-       :command-name "go-mod"
-       :command-line "go mod"
-       :display "Go: module maintenance")
-      (list
-       :command-name "go-work"
-       :command-line "go work"
-       :display "Go: workspace maintenance")
-      (list
-       :command-name "go-test"
-       :command-line "go test"
-       :display "Go: test packages")
-      (list
-       :command-name "go-tool"
-       :command-line "go tool"
-       :display "Go: run specified go tool")
-      (list
-       :command-name "go-version"
-       :command-line "go version"
-       :display "Go: print Go version")
-      (list
-       :command-name "go-vet"
-       :command-line "go vet"
-       :display "Go: report likely mistakes in packages")))))
-
 (defun run-command-recipes-go-p ()
   "Return non-nil value when the recipe for Go should work."
-  (-contains-p run-command-recipes-go-major-modes
-               major-mode))
+  (memq major-mode
+        run-command-recipes-go-major-modes))
+
+(defun run-command-recipes-go ()
+  "Recipe of `run-command' for go."
+  (when (run-command-recipes-go-p)
+    (let ((dir (run-command-recipes-project-root)))
+      (list
+       (list
+        :command-name "go-build-project"
+        :command-line "go build"
+        :working-dir dir
+        :display "Go: compile packages and dependencies")
+       (and
+        (buffer-file-name)
+        (list
+         :command-name "go-build"
+         :display "Go: build file"
+         :working-dir dir
+         :command-line (concat "go build " (buffer-file-name))))
+       (and
+        (buffer-file-name)
+        (list
+         :command-name "go-run"
+         :display "Go: compile, execute file"
+         :working-dir dir
+         :command-line (compile "go run " (buffer-file-name))))
+       (list
+        :command-name "go-run-project"
+        :command-line "go run"
+        :working-dir dir
+        :display "Go: compile, execute program")
+       (list
+        :command-name "go-bug"
+        :command-line "go bug"
+        :working-dir dir
+        :display "Go: start a bug report")
+       (list
+        :command-name "go-clean"
+        :command-line "go clean"
+        :working-dir dir
+        :display "Go: remove object files and cached files")
+       (list
+        :command-name "go-clean-recursively"
+        :command-line "go clean -r"
+        :working-dir dir
+        :display "Go: remove object files and cached files recursively")
+       (list
+        :command-name "go-env"
+        :command-line "go env"
+        :working-dir dir
+        :display "Go: print Go environment information")
+       (list
+        :command-name "go-fix"
+        :command-line "go fix"
+        :working-dir dir
+        :display "Go: update packages to use new APIs")
+       (list
+        :command-name "go-fmt-project"
+        :command-line "go fmt"
+        :working-dir dir
+        :display "Go: gofmt (reformat) package sources")
+       (and
+        (buffer-file-name)
+        (list
+         :command-name "go-fmt"
+         :command-line (concat "go fmt " (buffer-file-name))
+         :working-dir dir
+         :display "Go: gofmt (reformat) file"))
+       (list
+        :command-name "go-generate"
+        :command-line "go generate"
+        :working-dir dir
+        :display "Go: generate Go files by processing source")
+       (list
+        :command-name "go-get"
+        :command-line "go get"
+        :working-dir dir
+        :display "Go: add dependencies to current module and install them")
+       (list
+        :command-name "go-install"
+        :command-line "go install"
+        :working-dir dir
+        :display "Go: compile and install packages and dependencies")
+       (list
+        :command-name "go-list"
+        :command-line "go list"
+        :working-dir dir
+        :display "Go: list packages or modules")
+       (list
+        :command-name "go-mod"
+        :command-line "go mod"
+        :working-dir dir
+        :display "Go: module maintenance")
+       (list
+        :command-name "go-work"
+        :command-line "go work"
+        :working-dir dir
+        :display "Go: workspace maintenance")
+       (list
+        :command-name "go-test"
+        :command-line "go test"
+        :working-dir dir
+        :display "Go: test package")
+       (list
+        :command-name "go-tool"
+        :command-line "go tool"
+        :working-dir dir
+        :display "Go: run specified go tool")
+       (list
+        :command-name "go-version"
+        :command-line "go version"
+        :working-dir dir
+        :display "Go: print Go version")
+       (list
+        :command-name "go-vet"
+        :command-line "go vet"
+        :working-dir dir
+        :display "Go: report likely mistakes in packages")))))
 
 (provide 'run-command-recipes-go)
 ;;; run-command-recipes-go.el ends here
